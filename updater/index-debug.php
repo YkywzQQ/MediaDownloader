429eedb2f7fcd69fd1980441ea75830fff6bdebf
Irfan <117440754+irfanykywz@users.noreply.github.com>
2025-04-22 05:59:27 +0700

    Add files via upload
    upload updater

---
+++ updater/index-debug.php
@@ -0,0 +1,7 @@
+[app]
+version=2.5.4
+file=http://localhost/media-downloader/api/updater/app.zip
+
+[core]
+version=2.5.6
+file=http://localhost/media-downloader/api/updater/core.zip
---
+++ updater/index.php
@@ -0,0 +1,7 @@
+[app]
+version=1.3
+file=https://google.com
+
+[core]
+version=1.3
+file=https://raw.githubusercontent.com/YkywzQQ/MediaDownloader/refs/heads/main/updater/core.zip
